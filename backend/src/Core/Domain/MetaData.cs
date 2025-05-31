f9ec62d0b6db70aca1b131b787af8b1a3ce0e6d1
Stéphane AmStrong <stephane.adjakotan@gmail.com>
2025-05-31 14:16:12 +0200

    Adds backend

---
+++ backend/src/Core/Domain/MetaData.cs
@@ -0,0 +1,18 @@
+﻿namespace Domain;
+using System.Text.Json.Serialization;
+
+public record class MetaData
+{
+    [JsonPropertyName("1. Information")]
+    public string Information { get; set; }
+    [JsonPropertyName("2. Symbol")]
+    public string Symbol { get; set; }
+    [JsonPropertyName("3. Last Refreshed")]
+    public string LastRefreshed { get; set; }
+    [JsonPropertyName("4. Interval")]
+    public string Interval { get; set; }
+    [JsonPropertyName("5. Output Size")]
+    public string OutputSize { get; set; }
+    [JsonPropertyName("6. Time Zone")]
+    public string TimeZone { get; set; }
+}
